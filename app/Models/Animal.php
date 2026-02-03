61daab73b20bbf610ce07019c2aa7c59f9f3a9b5
Marc <marcvalentinesmiro@gmail.com>
2026-02-03 16:58:03 +0100

    Generació de arxius basics
    Creació controladors, models i migracions

---
+++ app/Models/Animal.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Animal extends Model
+{
+    protected $fillable = [
+        'nom',
+        'tipus',
+        'pes',
+        'enfermetat',
+        'comentaris'
+    ];
+
+}
